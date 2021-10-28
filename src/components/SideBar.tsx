f6611f27a1e7927fb09667340238591ddd2ec8db
GabrielSena100 <gabrielsena500@gmail.com>
2021-10-28 12:27:41 -0300

    Final Commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+
+
+interface ISideBarProps {
+  selectedGenreId: number;
+  filmsByGenreId: (genreId: number) => void;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({selectedGenreId, filmsByGenreId}: ISideBarProps) {
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => filmsByGenreId(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
